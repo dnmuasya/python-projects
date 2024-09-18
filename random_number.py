07ce7becbe67b25b10e7ec4cb02593e5eeccf70a
Doris <152690075+dnmuasya@users.noreply.github.com>
2024-09-18 13:20:35 +0300

    Add files via upload

---
+++ random_number.py
@@ -0,0 +1,33 @@
+""" Module for random numbers"""
+import random
+
+number = random.randint(1,25)
+number_of_guesses = 0
+
+while number_of_guesses < 5:
+    try:
+        guess = input('Guess a number between 1 and 25: ')
+        guess = int(guess)
+        number_of_guesses += 1
+        if guess > 25:
+            print('Guess out of scope!!')
+        elif guess < number:
+            print('Guess value too low!')
+        elif guess > number:
+            print('Guess value too high!')
+        
+        else:
+            print('Correct!!you guessed the number in '+str(number_of_guesses)+ ' tries!')
+            break
+    except ValueError:
+        print('Please enter a valid number!')
+
+    if guess == number:
+        break
+
+    elif number_of_guesses == 5:
+        print('You did not guess the number in ' + str(number_of_guesses) + ' tries! The number was ' + str(number) + '.')
+        break
+    else:
+        print('Try again.')
+
