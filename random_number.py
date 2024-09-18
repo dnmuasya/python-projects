e629761a37c011dc21cdb6f187541f5fb41181b6
Doris <152690075+dnmuasya@users.noreply.github.com>
2024-09-18 11:21:02 +0300

    Add files via upload
    #Generating a random number (1-25)
    #Create a counter to store the number of guesses made
    #While the number of guesses is less than 5:
    #if the guess equals the number
    #print the number, + the number of times to get the right guess and exit the game
    #if the number of guesses reaches 5 without getting it correct, print the number, and exit the game

---
+++ random_number.py
@@ -0,0 +1,23 @@
+""" Module for random numbers"""
+import random
+
+number = random.randint(1,25)
+number_of_guesses = 0
+
+while number_of_guesses < 5:
+    print('Guess a number between 1 and 25: ')
+    guess = input()
+    guess = int(guess)
+    number_of_guesses += 1
+
+    if guess == number:
+        break
+    elif number_of_guesses == 5:
+        break
+    else:
+        print('Nope! Try again.')
+
+if guess == number:
+    print('Correct! You guessed the number in ' + str(number_of_guesses)+ ' tries!')
+else:
+    print('You did not guess the number. The number was ' + str(number) + '.')
